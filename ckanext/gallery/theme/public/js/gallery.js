5ae4b570b246db4be5c36bb0a5dbbc1b0ab1679c
benscott <ben@benscott.co.uk>
2015-11-10 16:41:27 +0000

    Moved pop up into MAM

--- ckanext/gallery/theme/public/js/gallery.js
+++ ckanext/gallery/theme/public/js/gallery.js
@@ -9,22 +9,12 @@ ckan.module('gallery', function (jQuery, _) {
   return {
     initialize: function () {
         self = this
-        document.domain = "nhm.ac.uk";
         $("a", this.el).each(function(i) {
             $(this).data("index", i)
             $(this).on('click', jQuery.proxy(self._openLightbox));
         });
-
-//        $('#download-tooltip').tooltip({
-//            position: { of: '#blueimp-gallery a.gallery-control-download', my: 'left+60 center', at: 'left center' },
-//            tooltipClass: "download-tooltip"
-//        });
-
-//        $('#blueimp-gallery a.gallery-control-download').on('click', jQuery.proxy(self._showDownloadTooltip));
-//        // Hide the tooltip if the gallery is clicked
-//        $('#blueimp-gallery').on('click', jQuery.proxy(self._hideDownloadTooltip));
     },
-    _getCurrentImage: function () {
+    getCurrentImage: function () {
         var pos = self.gallery.getIndex();
         return self.options.images[pos]
     },
@@ -35,63 +25,52 @@ ckan.module('gallery', function (jQuery, _) {
 //                self._hideDownloadTooltip()
 //            },
             onslide: function () {
-                self._updateLinks()
+                self._onImageUpdate()
             }
         }
         self.gallery = blueimp.Gallery(self.options.images, options)
         e.stopPropagation();
         return false;
     },
-//    _showDownloadTooltip: function(e){
-//        var image = self._getCurrentImage()
-//        $('#download-tooltip').data("image", image).tooltip( "option", "content", self._getTooltipText(image)).tooltip('open');
-//        e.stopPropagation();
-//        return false;
-//    },
-//    _hideDownloadTooltip: function(e){
-//        $('#download-tooltip').tooltip('close');
-//    },
-//    _getTooltipText: function(image){
-//        // Get the HTML image 0 we then know the dimensions
-//        $img = $('#blueimp-gallery img[src="' + image.href + '"]')
-//        return '<ul><li><a href="' + image.href + '" download>Download image (' + $img[0].naturalHeight + '&times;' + $img[0].naturalWidth + ')</a></li></ul>'
-//    },
-    _updateLinks: function(){
-        var image = self._getCurrentImage()
+    _onImageUpdate: function(){
+        var image = self.getCurrentImage()
+        var $gallery = $('#blueimp-gallery')
+        // Set image data attribute
+        $gallery.data('image', image)
 
         // Update download link
         $('#blueimp-gallery a.gallery-control-download').attr('href', image.href);
-        $('#blueimp-gallery a.gallery-control-download').on('click', jQuery.proxy(self._downloadImage));
+//        $('#blueimp-gallery a.gallery-control-download').on('click', jQuery.proxy(self._downloadImage));
 
         if(image.copyright){
-            $('#blueimp-gallery .copyright').html(image.copyright)
+            $gallery.find('.copyright').html(image.copyright)
         }
         // If we have a link to record, update the link and show it
         // Otherwise, hide the link
         if(image.link){
-            $('#blueimp-gallery .gallery-control-link').attr('href', image.link).show()
+            $gallery.find('.gallery-control-link').attr('href', image.link).show()
         }else{
-            $('#blueimp-gallery .gallery-control-link').hide()
+            $gallery.find('.gallery-control-link').hide()
         }
     },
-    _downloadImage: function(e){
-        self.downloadFile('http://www.nhm.ac.uk/services/media-store/asset/2d63e01b999aaa0581397d9e629e4bc9f30677a7/contents/preview', function(blob) {
-            saveAs(blob, "image.png");
-        });
-        e.stopPropagation();
-        return false;
-    },
-    downloadFile: function(url, success){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = "blob";
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (success) success(xhr.response);
-            }
-        };
-       xhr.send(null);
-    },
+//    _downloadImage: function(e){
+//        self.downloadFile('http://www.nhm.ac.uk/services/media-store/asset/2d63e01b999aaa0581397d9e629e4bc9f30677a7/contents/preview', function(blob) {
+//            saveAs(blob, "image.png");
+//        });
+//        e.stopPropagation();
+//        return false;
+//    },
+//    downloadFile: function(url, success){
+//        var xhr = new XMLHttpRequest();
+//        xhr.open('GET', url, true);
+//        xhr.responseType = "blob";
+//        xhr.onreadystatechange = function () {
+//            if (xhr.readyState == 4) {
+//                if (success) success(xhr.response);
+//            }
+//        };
+//       xhr.send(null);
+//    },
     options: {
         images: []
     }
