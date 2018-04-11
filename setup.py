8a37dc58325fbce10ded3a43bc83c41fa2aa0eec
Alice Butcher <alycejenni@gmail.com>
2018-04-11 13:38:04 +0100

    tidied setup.py and changed version number

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 # This file is part of ckanext-gallery
 # Created by the Natural History Museum in London, UK
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-version = u'0.1'
+version = u'0.2'
 
 setup(
-	name=u'ckanext-gallery',
-	version=version,
-	description=u'',
-	long_description=u'''\
-	''',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords=u'',
-	license=u'',
-	packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
-	namespace_packages=[u'ckanext', u'ckanext.gallery'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	u'''
-        [ckan.plugins]
-            gallery = ckanext.gallery.plugins.gallery:GalleryPlugin
-            gallery_image = ckanext.gallery.plugins.image:GalleryImagePlugin
-	''',
-)
+    name=u'ckanext-gallery',
+    version=version,
+    description=u'',
+    long_description=u'',
+    classifiers=[],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords=u'',
+    license=u'',
+    packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
+    namespace_packages=[u'ckanext', u'ckanext.gallery'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        ],
+    entry_points= \
+        u'''
+            [ckan.plugins]
+                gallery = ckanext.gallery.plugins.gallery:GalleryPlugin
+                gallery_image = ckanext.gallery.plugins.image:GalleryImagePlugin
+        ''',
+    )
