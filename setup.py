6743a7f5f95645bd20ca63a503d6f7962c105445
Alice Butcher <alycejenni@gmail.com>
2019-07-23 18:22:49 +0100

    Standardise extension documentation/metadata

--- setup.py
+++ setup.py
@@ -6,28 +6,35 @@
 
 from setuptools import find_packages, setup
 
-version = u'0.2'
+__version__ = u'1.0.0-alpha'
+
+with open(u'README.md', u'r') as f:
+    __long_description__ = f.read()
 
 setup(
     name=u'ckanext-gallery',
-    version=version,
-    description=u'',
-    long_description=u'',
-    classifiers=[],
-    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords=u'',
-    license=u'',
-    packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
+    version=__version__,
+    description=u'A CKAN extension for a dataset gallery view.',
+    long_description=__long_description__,
+    classifiers=[
+        u'Development Status :: 3 - Alpha',
+        u'Framework :: Flask',
+        u'Programming Language :: Python :: 2.7'
+    ],
+    keywords=u'CKAN data gallery',
+    author=u'Natural History Museum',
+    author_email=u'data@nhm.ac.uk',
+    url=u'https://github.com/NaturalHistoryMuseum/ckanext-gallery',
+    license=u'GNU GPLv3',
+    packages=find_packages(exclude=[u'tests']),
     namespace_packages=[u'ckanext', u'ckanext.gallery'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        # -*- Extra requirements: -*-
-        ],
+    install_requires=[],
     entry_points= \
         u'''
-            [ckan.plugins]
-                gallery = ckanext.gallery.plugins.gallery:GalleryPlugin
-                gallery_image = ckanext.gallery.plugins.image:GalleryImagePlugin
+        [ckan.plugins]
+            gallery=ckanext.gallery.plugins.gallery:GalleryPlugin
+            gallery_image=ckanext.gallery.plugins.image:GalleryImagePlugin
         ''',
     )
